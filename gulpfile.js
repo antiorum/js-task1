ffb203cbff375a5855b3db4d5892dc50d78e3119
antiorum <antiorum@inbox.ru>
2020-07-23 22:14:51 +0400

    remove temp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,59 @@
-const {src, dest, parallel, series, watch} = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify-es').default;
+const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 
 function browsersync() {
-    browserSync.init({
-        server: {baseDir: 'app/'},
-        notify: false
-    })
+  browserSync.init({
+    server: { baseDir: 'app/' },
+    notify: false
+  });
 }
 
 function styles() {
-    return src('app/sass/main.sass')
-        .pipe(sass())
-        .pipe(concat('app.min.css'))
-        .pipe(dest('app/css/'))
+  return src('app/sass/main.sass')
+    .pipe(sass())
+    .pipe(concat('app.min.css'))
+    .pipe(cleanCSS())
+    .pipe(dest('app/css/'));
 }
 
 function scripts() {
-    return src([
-        'app/js/class.js',
-        'app/js/app.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(dest('app/js/'))
-        .pipe(browserSync.stream())
+  return src([
+    'app/js/class.js',
+    'app/js/app.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js/'))
+    .pipe(browserSync.stream());
 }
 
 function startwatch() {
-    watch(['app/sass/*.sass'], styles);
-    watch(['app/js/*.js', '!app/js/*.min.js'], scripts);
-    watch('app/*.html').on('change', browserSync.reload);
+  watch([ 'app/sass/*.sass' ], styles);
+  watch([ 'app/js/*.js', '!app/js/*.min.js' ], scripts);
+  watch('app/*.html').on('change', browserSync.reload);
 }
 
 function cleandist() {
-    return del('dist/**/*', {force: true});
+  return del('dist/**/*', { force: true });
 }
 
 function copybuild() {
-    return src([
-        'app/css/**/*.min.css',
-        'app/js/**/*.min.js',
-        'app/*.html'
-    ], {
-        base: 'app'
-    }).pipe(dest('dist'))
+  return src([
+    'app/css/**/*.min.css',
+    'app/js/**/*.min.js',
+    'app/*.html'
+  ], {
+    base: 'app'
+  }).pipe(dest('dist'));
 }
 
 exports.browsersync = browsersync;
+exports.styles = styles;
 exports.scripts = scripts;
 exports.build = series(cleandist, styles, scripts, copybuild);
 
