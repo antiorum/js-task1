52be9c21b6fcdd249f111829353c43fc17867713
antiorum <antiorum@inbox.ru>
2020-07-28 09:25:07 +0400

    Tasks 1-3

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify-es').default;
 const cleanCSS = require('gulp-clean-css');
+const pug = require('gulp-pug');
 const del = require('del');
 
 function browsersync() {
@@ -14,7 +15,7 @@ function browsersync() {
 }
 
 function styles() {
-  return src('app/sass/main.sass')
+  return src('app/sass/*.sass')
     .pipe(sass())
     .pipe(concat('app.min.css'))
     .pipe(cleanCSS())
@@ -23,8 +24,7 @@ function styles() {
 
 function scripts() {
   return src([
-    'app/js/class.js',
-    'app/js/app.js'
+    'app/js/*.js'
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -32,7 +32,15 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
+function compilepug() {
+  return src('app/pug/index.pug')
+    .pipe(pug())
+    .pipe(dest('app/'))
+    .pipe(browserSync.stream());
+}
+
 function startwatch() {
+  watch([ 'app/pug/*.pug' ], compilepug);
   watch([ 'app/sass/*.sass' ], styles);
   watch([ 'app/js/*.js', '!app/js/*.min.js' ], scripts);
   watch('app/*.html').on('change', browserSync.reload);
@@ -46,7 +54,8 @@ function copybuild() {
   return src([
     'app/css/**/*.min.css',
     'app/js/**/*.min.js',
-    'app/*.html'
+    'app/*.html',
+    'app/images/*.png'
   ], {
     base: 'app'
   }).pipe(dest('dist'));
@@ -55,6 +64,8 @@ function copybuild() {
 exports.browsersync = browsersync;
 exports.styles = styles;
 exports.scripts = scripts;
-exports.build = series(cleandist, styles, scripts, copybuild);
+exports.pug = compilepug;
 
-exports.default = parallel(styles, scripts, browsersync, startwatch);
+exports.build = series(cleandist, styles, scripts, compilepug, copybuild);
+
+exports.default = parallel(styles, scripts, compilepug, browsersync, startwatch);
